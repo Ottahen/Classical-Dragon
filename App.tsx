1751628e6d16dd7b5f23d5dfbf9cc144a2afa1b1
ℒ <cosmiclegendstudio@gmail.com>
2026-06-13 15:44:27 +0545

    Src

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Chat from './pages/Chat'
+import NotFound from './pages/NotFound'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
